3a18c369661ec77985699383a25b88424d4f31bc
Lalit Mane <99650346+Lalit-Mane@users.noreply.github.com>
2022-06-23 13:52:56 +0530

    Add files via upload

---
+++ Mainl.java
@@ -0,0 +1,32 @@
+package lalitGl.lab.driver;
+
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import lalitGl.lab.service.MergeSort;
+import lalitGl.lab.service.NotesCount;
+
+public class Mainl {
+
+	public static void main(String[] args) {
+
+		MergeSort mergeSort = new MergeSort();
+		NotesCount notesCount = new NotesCount();
+
+		System.out.println("enter the size of currency denominations ");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] notes = new int[size];
+		System.out.println("enter the currency denominations value");
+		for (int i = 0; i < size; i++) {
+			notes[i] = sc.nextInt();
+		}
+		System.out.println("enter the amount you want to pay");
+		int amount = sc.nextInt();
+		mergeSort.sort(notes,0,notes.length-1);
+		notesCount.notesCountImplementation(notes, amount);
+	}
+}
+
